243581e5018e23ac9d325c7b8dc54318d95fa417
Rodolfo Alvarenga <rodolfo.alvarenga@br.bosch.com>
2023-07-14 10:13:08 -0300

    Finished project

---
+++ src/main/java/com/algaworks/algatransito/api/controller/VeiculoController.java
@@ -0,0 +1,70 @@
+package com.algaworks.algatransito.api.controller;
+
+import com.algaworks.algatransito.api.assembler.VeiculoAssembler;
+import com.algaworks.algatransito.api.model.VeiculoModel;
+import com.algaworks.algatransito.api.model.input.VeiculoInput;
+import com.algaworks.algatransito.domain.model.Veiculo;
+import com.algaworks.algatransito.domain.repository.VeiculoRepository;
+import com.algaworks.algatransito.domain.service.ApreensaoVeiculoService;
+import com.algaworks.algatransito.domain.service.RegistroVeiculoService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/veiculos")
+public class VeiculoController {
+
+    private final ApreensaoVeiculoService apreensaoVeiculoService;
+    private final RegistroVeiculoService registroVeiculoService;
+    private final VeiculoRepository veiculoRepository;
+    private final VeiculoAssembler veiculoAssembler;
+
+    @GetMapping
+    public List<VeiculoModel> listar() {
+        return veiculoAssembler.toCollectionModel(veiculoRepository.findAll());
+    }
+
+    @GetMapping("/{veiculoId}")
+    public ResponseEntity<VeiculoModel> buscar(@PathVariable Long veiculoId) {
+        return veiculoRepository.findById(veiculoId)
+                .map(veiculoAssembler::toModel)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public VeiculoModel cadastrar(@Valid @RequestBody VeiculoInput veiculoInput) {
+        Veiculo novoVeiculo = veiculoAssembler.toEntity(veiculoInput);
+        Veiculo veiculoCadastrado = registroVeiculoService.cadastrar(novoVeiculo);
+
+        return veiculoAssembler.toModel(veiculoCadastrado);
+    }
+
+    @PutMapping("/{veiculoId}/apreensao")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void apreender(@PathVariable Long veiculoId) {
+        apreensaoVeiculoService.apreender(veiculoId);
+    }
+
+    @DeleteMapping("/{veiculoId}/apreensao")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removerApreensao(@PathVariable Long veiculoId) {
+        apreensaoVeiculoService.removerApreensao(veiculoId);
+    }
+
+}
---
+++ src/main/java/com/algaworks/algatransito/domain/model/Proprietario.java
@@ -0,0 +1,40 @@
+package com.algaworks.algatransito.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Proprietario {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 60)
+    private String nome;
+
+    @NotBlank
+    @Size(max = 255)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(max = 20)
+    @Column(name = "fone")
+    private String telefone;
+
+}
